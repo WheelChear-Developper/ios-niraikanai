35afc8c125c39d4f856c2cb33d55f8b6299943ad
SMARTTECNO <amatani@smarttecno.net>
2014-02-23 20:27:56 +0900

    Initial Commit

---
+++ niraikanai/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  niraikanai
+//
+//  Created by SMARTTECNO. on 2014/02/23.
+//  Copyright (c) 2014年 akafune, inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
